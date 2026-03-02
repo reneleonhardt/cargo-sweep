32c0c125c11c4e81f41f7984bc19a9a74a366a33
Rene Leonhardt <65483435+reneleonhardt@users.noreply.github.com>
2026-03-02 12:57:06 +0100

    fix: print total_cleaned after all loops

--- src/main.rs
+++ src/main.rs
@@ -262,10 +262,10 @@ fn main() -> anyhow::Result<()> {
                 Err(e) => error!("Failed to clean {project_path:?}: {e:?}"),
             };
         }
+    }
 
-        if processed_paths.len() > 1 {
-            info!("Total amount: {}", format_bytes(total_cleaned));
-        }
+    if total_cleaned > 0 {
+        info!("Total amount: {}", format_bytes(total_cleaned));
     }
 
     Ok(())
